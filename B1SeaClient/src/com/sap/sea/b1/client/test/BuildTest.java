4c2884571409a364399304ff910901257a73f125
templefox <unboundfox@163.com>
2015-01-04 22:19:38 +0800

    UT for build

--- B1SeaClient/src/com/sap/sea/b1/client/test/BuildTest.java
+++ B1SeaClient/src/com/sap/sea/b1/client/test/BuildTest.java
@@ -31,47 +31,29 @@ import com.spotify.docker.client.messages.RemovedImage;
 
 public class BuildTest {
 	@Test
-	public void getImageID(){
+	public void getImageID() {
 		BuildLog log = new BuildLog("Removing intermediate container 060b6428bd07 Successfully built 455f2b7f013e");
-		
+
 		Assert.assertTrue(log.getImageID().equals("455f2b7f013e"));
 	}
-	
+
 	@Test
-	public void buildTest(){
+	public void buildTest() {
 		SeaClient client = new SeaClient("localhost:8080/Sea");
-		
+
 		IslandInfo info = new IslandInfo("192.168.1.106:7777");
-		
+
 		Island island = client.getIsland(info);
-		
+
 		BuildLog log = island.node().build("/home/dockerbuild");
 		System.out.println(log);
-		Assert.assertFalse(log==null);
-		
+		Assert.assertFalse(log == null);
+
 		String id = log.getImageID();
-		Assert.assertFalse(id==null);
+		Assert.assertFalse(id == null);
 		Assert.assertFalse(id.isEmpty());
-		
-		island.docker().removeImage(id);
-	}
-	  private static final ClientConfig DEFAULT_CONFIG = new ClientConfig(
-		      ObjectMapperProvider.class,
-		      JacksonFeature.class,
-		      LogsResponseReader.class,
-		      ProgressResponseReader.class);
-	@Test
-	public void redirectTest(){       
-		
-		final ClientConfig config = DEFAULT_CONFIG
-	.connectorProvider(new ApacheConnectorProvider());
-		Client client = ClientBuilder.newClient(config);
-		WebTarget webTarget = client.target("http://localhost:8080/Sea/island/192.168.1.106:7777/docker/v1.12/images/4f9066f37bd1?force=true&noprune=false");
-		try {
-			System.out.println(webTarget.request().async().method("DELETE", new GenericType<List<RemovedImage>>() {}).get());
-		} catch (InterruptedException | ExecutionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
+		List<String> removedImages = island.docker().removeImage(id);
+		Assert.assertTrue(removedImages.get(0).startsWith(id));
 	}
 }
